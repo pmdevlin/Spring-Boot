bd619961b7a341415914a2efe5dc4e225450fb03
Paul Devlin <pmdevlin360@gmail.com>
2022-08-31 13:00:55 -0500

    new project

---
+++ react-connection/src/main/java/com/connectreact/reactconnection/ReactConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.connectreact.reactconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactConnectionApplication.class, args);
+	}
+
+}
---
+++ react-connection/src/test/java/com/connectreact/reactconnection/ReactConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectreact.reactconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
